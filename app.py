d35b3834fd10a700bbb14c6c35c315320bab13ff
Alvif Sandana Mahardika <55418227+alvifsandana@users.noreply.github.com>
2021-03-22 09:03:29 +0700

    selesai part 1 :)

---
+++ app.py
@@ -0,0 +1,26 @@
+import requests
+import bs4
+
+url = 'http://www.jadwalsholat.pkpu.or.id/?id=26'
+contents = requests.get(url)
+
+response = bs4.BeautifulSoup(contents.text, features='html.parser')
+
+data = response.find_all('tr', 'table_highlight')
+data = data[0]
+
+sholat = {}
+i = 0
+for d in data:
+    if i == 1:
+        sholat['subuh'] = d.get_text()
+    elif i == 2:
+        sholat['dhuhur'] = d.get_text()
+    elif i == 3:
+        sholat['ashar'] = d.get_text()
+    elif i == 4:
+        sholat['magrib'] = d.get_text()
+    elif i == 5:
+        sholat['isya'] = d.get_text()
+    i += 1
+print(sholat)
